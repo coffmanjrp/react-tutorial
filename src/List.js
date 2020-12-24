4826ad775c23192155422337b1854c481663ad23
coffmanjrp <coffmanjrp@gmail.com>
2020-12-23 17:48:09 -0800

    component lifecycle tutorial

--- src/List.js
+++ src/List.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+
 export const List = ({ langs }) => {
+  useEffect(() => {
+    console.log('Listjs:useEffect');
+
+    return () => {
+      console.log('Listjs:useEffect:unmount');
+    };
+  });
+
   return (
     <div>
       {langs.map((lang, index) => {
